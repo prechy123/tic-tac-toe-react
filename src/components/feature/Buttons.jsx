9c0c9c20715e60b21a23aa51230e48466e77865c
prechy123 <bamideleprecious100@gmail.com>
2023-08-29 15:19:31 +0100

    Divided buttons in row in which there are three per column and row

--- src/components/feature/Buttons.jsx
+++ src/components/feature/Buttons.jsx
@@ -4,14 +4,21 @@ import Button from "./Button";
 export default function Buttons() {
   return (
     <>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <div className="row">
+        <Button />
+        <Button />
+        <Button />
+      </div>
+      <div className="row">
+        <Button />
+        <Button />
+        <Button />
+      </div>
+      <div className="row">
+        <Button />
+        <Button />
+        <Button />
+      </div>
     </>
   );
 }
