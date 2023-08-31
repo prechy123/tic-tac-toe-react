56c1fc914bd6b97bb1e1878751e4ef615ea091a3
prechy123 <bamideleprecious100@gmail.com>
2023-08-31 14:26:59 +0100

    Using font mono

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import Buttons from "./feature/Buttons";
 
 export default function Home() {
   return (
-    <section class="flex justify-center items-center h-screen bg-gray-600">
+    <section class="flex justify-center items-center h-screen bg-gray-600 font-mono">
       <div class="text-center">
         <h2>TIK - TIC - TOE</h2>
         <Buttons />
