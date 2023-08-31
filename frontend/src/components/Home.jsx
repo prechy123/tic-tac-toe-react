52eb2b52bd2e4248fe3b127ae5b8dc4693305cb5
prechy123 <bamideleprecious100@gmail.com>
2023-08-31 14:28:57 +0100

    Styles tic tac toe div

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,7 @@ import Buttons from "./feature/Buttons";
 export default function Home() {
   return (
     <section class="flex justify-center items-center h-screen bg-gray-600 font-mono">
-      <div class="text-center">
+      <div class="text-center bg-gray-300 rounded p-4 shadow-lg">
         <h2>TIK - TIC - TOE</h2>
         <Buttons />
       </div>
