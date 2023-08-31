62c98c328e943df3894986b4e3d2d00b38b975a2
prechy123 <bamideleprecious100@gmail.com>
2023-08-31 14:33:15 +0100

    Made title bold

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <section className="flex justify-center items-center h-screen bg-gray-600 font-mono">
       <div className="text-center bg-gray-300 rounded p-4 shadow-lg">
-        <h2 className="">TIK - TIC - TOE</h2>
+        <h2 className="font-bold">TIK - TIC - TOE</h2>
         <Buttons />
       </div>
     </section>
